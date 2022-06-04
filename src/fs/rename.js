a3ace9ff5e0c2ec1f1df5f8fb70fa2e5a004462e
Alexey Pirozerskiy <pirozerskiy.a@wb.ru>
2022-06-04 17:46:32 +0300

    feat:all fs

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,20 @@
+import fs  from 'fs';
+const fsPromises = fs.promises;
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const rename = async () => {
-    // Write your code here 
+    const filePath= path.join(__dirname, 'files', 'wrongFilename.txt');
+    const newFilePath= path.join(__dirname, 'files', 'properFilename.md');
+    try{
+        await fsPromises.open(filePath,'wx');
+        await fsPromises.rename(filePath,newFilePath);
+    }
+    catch (e) {
+        throw new Error('FS operation failed');
+    }
 };
+rename();
