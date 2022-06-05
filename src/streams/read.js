0c862ee2da469cf3647679e12bfaeec63b74d51b
Alexey Pirozerskiy <pirozerskiy.a@wb.ru>
2022-06-05 14:28:16 +0300

    feat:sterams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const read = async () => {
-    // Write your code here 
+
+    const { stdout } = process;
+    const filePath = path.join(__dirname, 'files', 'fileToRead.txt');
+    const readableStream =  fs.createReadStream(filePath,'utf-8');
+    readableStream.on('data', chunk => stdout.write(chunk));
 };
+read();
